ed8b57b55a32ad5faf6ca366541d92a0832cdb1b
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2020-04-19 20:55:58 -0500

    feat: watch mode

--- src/cli.ts
+++ src/cli.ts
@@ -11,6 +11,8 @@ import { installModule } from ".";
 import { loadYaml } from "./loaders/yaml";
 import { runTest, setupTests } from "./objects/test";
 import * as tape from "tape";
+import { watch } from "chokidar";
+import * as glob from "glob";
 
 interface CliOpts {
   database: string;
@@ -18,6 +20,7 @@ interface CliOpts {
   schema: string;
   out?: string;
   testFiles: string;
+  watch: boolean;
 }
 
 const install = async (argv: CliOpts) => {
@@ -25,52 +28,82 @@ const install = async (argv: CliOpts) => {
 
   const client = await pool.connect();
 
-  const m = await loadYaml({
-    include: argv.files,
-  });
+  const loadAndInstallModule = async () => {
+    const m = await loadYaml({
+      include: argv.files,
+    });
 
-  const runner: Runner =
-    argv.out === undefined ? (true ? directRunner : directRunner) : fileRunner;
+    const runner: Runner =
+      argv.out === undefined
+        ? true
+          ? directRunner
+          : directRunner
+        : fileRunner;
 
-  const context: RunContextI | ToFileRunContextI = {
-    schema: argv.schema,
-    client,
-    outFile: argv.out,
+    const context: RunContextI | ToFileRunContextI = {
+      schema: argv.schema,
+      client,
+      outFile: argv.out,
+    };
+
+    await installModule(m, runner, context);
   };
 
-  await installModule(m, runner, context);
+  await loadAndInstallModule();
+
+  if (argv.watch) {
+    const files: string[] = await new Promise((resolve, _reject) =>
+      glob(argv.files, (_err: any, f: string[]) => resolve(f)),
+    );
+
+    watch(files).on("all", loadAndInstallModule);
+  }
 };
 
 const test = async (argv: CliOpts) => {
   const pool = new Pool({ connectionString: argv.database });
 
-  const client = await pool.connect();
+  const installModuleAndRunTests = async () => {
+    const client = await pool.connect();
 
-  const m = await loadYaml({
-    include: argv.files,
-  });
+    const m = await loadYaml({
+      include: argv.files,
+    });
 
-  const runner = directRunner;
+    const runner = directRunner;
 
-  const runContext: RunContextI | ToFileRunContextI = {
-    schema: argv.schema,
-    client,
+    const runContext: RunContextI | ToFileRunContextI = {
+      schema: argv.schema,
+      client,
+    };
+
+    const testContext = {
+      runContext,
+      runner,
+    };
+
+    const reset = await setupTests(m, testContext);
+
+    for (const test of m.tests || []) {
+      await runTest(test, testContext, reset);
+    }
+
+    if (!argv.watch) {
+      tape.onFinish(() => {
+        setTimeout(process.exit, 30);
+      });
+    }
   };
 
-  const testContext = {
-    runContext,
-    runner,
-  };
+  await installModuleAndRunTests();
 
-  const reset = await setupTests(m, testContext);
+  if (argv.watch) {
+    const files: string[] = await new Promise((resolve, _reject) =>
+      glob(argv.files, (_err: any, f: string[]) => resolve(f)),
+    );
 
-  for (const test of m.tests || []) {
-    await runTest(test, testContext, reset);
+    watch(files).on("all", installModuleAndRunTests);
   }
-
-  tape.onFinish(() => {
-    setTimeout(process.exit, 30);
-  });
 };
 
 yargs
@@ -98,6 +131,12 @@ yargs
       demandOption: false,
       describe: "optional output file to write a migration too",
       type: "string",
+    },
+    watch: {
+      alias: "w",
+      describe: "watch for changes to files and rerun command",
+      type: "boolean",
+      default: false,
     },
   })
   .command({
